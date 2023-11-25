18fc1e2ea3a6c3d565a2ca5444aa45073a7d327c
motoyannn <motoya.kon@gmail.com>
2023-11-25 15:11:32 +0900

    chore: remove unused codebase

--- packages/motion/src/app/notes/layout.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function DocumentLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div>{children}</div>;
-}
--- packages/motion/src/app/notes/new/layout.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function DocumentNewLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div>{children}</div>;
-}
