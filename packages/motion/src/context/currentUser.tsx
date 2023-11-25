e937395d7b9a442d608f2a88e3d55788b3406e3d
motoyannn <motoya.kon@gmail.com>
2023-11-25 15:10:11 +0900

    chore: remove unused codebase

--- packages/motion/src/context/currentUser.tsx
+++
@@ -1,8 +0,0 @@
-import { User } from "@/.generate/gql";
-import { createContext, useContext } from "react";
-
-export const CurrentUserContext = createContext<User | null>(null);
-
-export const useCurrentUser = (): User | null => {
-  return useContext(CurrentUserContext);
-};
